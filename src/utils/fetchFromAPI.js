7b453b78c2ea2e7dcd468636b9aa73f3cd825ea6
HarshMehta2522 <maheta.hrs2522@gmail.com>
2023-08-08 17:23:56 +0530

    MPY

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': '59a7d4d751mshf4fab1e92499f24p1a20eajsn32ae62de059f',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
